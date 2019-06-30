aef2e5e9639b28a4ba887ec381401d3ed274ccb4
Aman Sharma <amansharma.11@hotmail.com>
2019-06-30 23:06:09 +0530

    commit

--- app/src/main/java/com/example/customalertdialog_passargumentsparcelable_callbackinterface/CustomDialogFragment.java
+++ app/src/main/java/com/example/customalertdialog_passargumentsparcelable_callbackinterface/CustomDialogFragment.java
@@ -1,6 +1,39 @@
 package com.example.customalertdialog_passargumentsparcelable_callbackinterface;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class CustomDialogFragment extends DialogFragment {
+
+    private Button cancelBtn,sendBtn;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view  = inflater.inflate(R.layout.custom_dialog_fragment,container,false);
+
+        cancelBtn = view.findViewById(R.id.cancel_button);
+        sendBtn = view.findViewById(R.id.send_button);
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        return view;
+    }
 }
--- app/src/main/java/com/example/customalertdialog_passargumentsparcelable_callbackinterface/MainActivity.java
+++ app/src/main/java/com/example/customalertdialog_passargumentsparcelable_callbackinterface/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.customalertdialog_passargumentsparcelable_callbackinterface;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.floating_action_button);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+    }
+
+    private void showDialog(){
+        CustomDialogFragment customDialogFragment = new CustomDialogFragment();
+        customDialogFragment.setCancelable(false);
+        customDialogFragment.show(getSupportFragmentManager(),"CUSTOM_ALERT_DIALOG");
     }
 }
