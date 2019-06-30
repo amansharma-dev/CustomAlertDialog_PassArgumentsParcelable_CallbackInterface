5b04fa72b4fdda70a5ac3af997529a7e87491039
Aman Sharma <amansharma.11@hotmail.com>
2019-06-30 22:18:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/customalertdialog_passargumentsparcelable_callbackinterface/CustomDialogFragment.java
@@ -0,0 +1,6 @@
+package com.example.customalertdialog_passargumentsparcelable_callbackinterface;
+
+import android.support.v4.app.DialogFragment;
+
+public class CustomDialogFragment extends DialogFragment {
+}
